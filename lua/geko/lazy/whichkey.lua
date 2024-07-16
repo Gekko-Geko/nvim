a63e9823290bb519e678c0a845f5923f934ab156
Gekko-Geko <gekko@geko.boo>
2024-07-16 10:18:46 +0200

    Update whichkey config

--- lua/geko/lazy/whichkey.lua
+++ lua/geko/lazy/whichkey.lua
@@ -19,72 +19,44 @@ return {
             vim.cmd("!docker compose down -v; docker compose build; docker compose up -d --force-recreate")
         end
 
-        require('which-key').register({
-            d = {
-                name = "Docker",
-                i = { docker_build, "Build docker image" },
-                c = { docker_compose_up, "Docker compose down/build/up" }
-
-            },
-            e = { "<cmd>Neotree toggle<CR>", "Toggle Neotree" },
-            f = { vim.lsp.buf.format, "Code format" },
-            h = {
-                name = "Harpoon",
-                a = { function()
-                    require("harpoon.mark").add_file()
-                end, "Harpoon add file" },
-                h = { function()
-                    require("harpoon.ui").toggle_quick_menu()
-                end, "Harpoon quick menu" },
-                n = { function()
-                    require("harpoon.ui").nav_next();
-                end, "Harpoon next file" },
-                p = { function()
-                    require("harpoon.ui").nav_prev();
-                end, "Harpoon previous file" },
-            },
-            j = { "<cmd>lprev<CR>zz", "which_key_ignore" },
-            k = { "<cmd>lnext<CR>zz", "which_key_ignore" },
-            m = { "<cmd>CellularAutomaton make_it_rain<CR>", "Make it rain" },
-            p = {
-                name = "Telescope",
-                f = { builtin.find_files, "Find files" },
-                p = { builtin.buffers, "Open buffers" },
-                s = { function()
-                    builtin.grep_string({ search = vim.fn.input("Grep > ") })
-                end, "Search string" },
-                h = { builtin.help_tags, "Help tags" }
-            },
-            s = { [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], "Rename under cursor" },
-            t = {
-                name = "Trouble",
-                t = { function()
-                    require("trouble").toggle()
-                end, "Toggle trouble" },
-                n = { function()
-                    require("trouble").next({ skip_groups = true, jump = true });
-                end, "Toggle trouble" },
-                p = { function()
-                    require("trouble").previous({ skip_groups = true, jump = true });
-                end, "Toggle trouble" },
-            },
-            u = { "<cmd>UndotreeToggle<CR>", "Toggle Undotree" },
-            y = { [["+y]], "which_key_ignore" },
-            Y = { [["+Y]], "which_key_ignore" },
-            z = {
-                z = { function()
-                    require("zen-mode").setup {
-                        window = {
-                            width = 120,
-                            options = {}
-                        },
-                    }
+        require('which-key').add({
+            { "<leader>d",  group = "Docker" },
+            { "<leader>dc", docker_compose_up,                                                                desc = "Docker compose down/build/up" },
+            { "<leader>di", docker_build,                                                                     desc = "Build docker image" },
+            { "<leader>e",  "<cmd>Neotree toggle<CR>",                                                        desc = "Toggle Neotree" },
+            { "<leader>f",  vim.lsp.buf.format,                                                               desc = "Code format" },
+            { "<leader>h",  group = "Harpoon" },
+            { "<leader>ha", function() require("harpoon.mark").add_file() end,                                desc = "Harpoon add file" },
+            { "<leader>hh", function() require("harpoon.ui").toggle_quick_menu() end,                         desc = "Harpoon quick menu" },
+            { "<leader>hn", function() require("harpoon.ui").nav_next(); end,                                 desc = "Harpoon next file" },
+            { "<leader>hp", function() require("harpoon.ui").nav_prev(); end,                                 desc = "Harpoon previous file" },
+            { "<leader>j",  "<cmd>lprev<CR>zz",                                                               hidden = true },
+            { "<leader>k",  "<cmd>lnext<CR>zz",                                                               hidden = true },
+            { "<leader>m",  "<cmd>CellularAutomaton make_it_rain<CR>",                                        desc = "Make it rain" },
+            { "<leader>p",  group = "Telescope" },
+            { "<leader>pf", builtin.find_files,                                                               desc = "Find files" },
+            { "<leader>ph", builtin.help_tags,                                                                desc = "Help tags" },
+            { "<leader>pp", builtin.buffers,                                                                  desc = "Open buffers" },
+            { "<leader>ps", function() builtin.grep_string({ search = vim.fn.input("Grep > ") }) end,         desc = "Search string" },
+            { "<leader>s",  ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>",                           desc = "Rename under cursor" },
+            { "<leader>t",  group = "Trouble" },
+            { "<leader>tn", function() require("trouble").next({ skip_groups = true, jump = true }); end,     desc = "Toggle trouble" },
+            { "<leader>tp", function() require("trouble").previous({ skip_groups = true, jump = true }); end, desc = "Toggle trouble" },
+            { "<leader>tt", function() require("trouble").toggle() end,                                       desc = "Toggle trouble" },
+            { "<leader>u",  "<cmd>UndotreeToggle<CR>",                                                        desc = "Toggle Undotree" },
+            { "<leader>y",  '"+y',                                                                            hidden = true },
+            {
+                "<leader>zz",
+                function()
+                    require("zen-mode").setup { window = { width = 120, options = {} }, }
                     require("zen-mode").toggle()
                     vim.wo.wrap = false
                     vim.wo.number = false
                     vim.wo.rnu = false
-                end, "Toggle Zenmode" },
+                end,
+                desc = "Toggle Zenmode"
             },
-        }, { prefix = "<leader>" })
+            { "<leader>Y", '"+Y', hidden = true }
+        })
     end
 }
